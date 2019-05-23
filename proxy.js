032433a66605fe958365d8b2da157233187f6d3c
Dirceu Belem <dirceu@fourtime.com>
2019-05-23 11:24:57 -0300

    Adicionando projeto

---
+++ proxy.js
@@ -0,0 +1,7 @@
+var http = require('http');
+var httpProxy = require('http-proxy');
+var proxy = httpProxy.createProxyServer({});
+
+http.createServer(function(req, res) {
+    proxy.web(req, res, { target: 'http://www.damadale.com.br/' });
+}).listen(13000);
